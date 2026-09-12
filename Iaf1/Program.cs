6f0e343f3c6f978cc91c0be04b79d86569a7520d
Gera <Gera@511-05>
2026-09-12 14:43:20 +0200

    Добавьте файлы проекта.

---
+++ Iaf1/Program.cs
@@ -0,0 +1,23 @@
+﻿////Console.Write("Введите ваше имя");
+////string name = Console.ReadLine();
+////Console.WriteLine("Меня зовут "+name);
+////Console.WriteLine("Меня зовут {0}",name);
+//////Console.WriteLine($"Меня зовут {name}");
+
+//Console.WriteLine("Введиите x");
+//int x  = int.Parse(Console.ReadLine());
+//Console.Write("Введите y");
+//int y = Convert.ToInt32(Console.ReadLine());
+//int s = x + y;
+//Console.WriteLine($"{x}+{y}+{s}");
+
+//float x = 3.7F;
+//double y = Math.Pow(x, 7);
+//Console.Write("Введите угол в градусах");
+//double angle = double.Parse(Console.ReadLine());
+//double y = Match.Sin(angle*Math.PI / 180);
+//Console.WriteLine($"y={y={y:f2}");
+
+double number = double.Parse(Console.ReadLine());
+double y=Math.Exp(7*number);
+Console.WriteLine($"y={y:f2}");
---
+++ lab1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.Write("Введите h:");
+double h = double.Parse(Console.ReadLine());
+double A = (Math.Tan(Math.Pow(y,3)- Math.Pow(h,4))+ Math.Pow(h,2)) / (Math.Pow(Math.Sin(h), 3)+y);
+Console.WriteLine($"A={A:F2}");
+
