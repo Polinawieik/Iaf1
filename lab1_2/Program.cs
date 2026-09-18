856ad340c498bb15c2c76c90de5db9457723782b
Gera <Gera@511-05>
2026-09-18 18:29:30 +0200

    Добавьте файлы проекта.

---
+++ lab1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿try
+{
+    Console.Write("Введите первую переменную");
+    double C = double.Parse(Console.ReadLine());
+    Console.Write("Введите вторую переменную");
+    double L = double.Parse(Console.ReadLine());
+    double T = 2 * Math.PI * Math.Sqrt(L * C);
+    double v = 1 / T;
+
+
+    Console.WriteLine($"Ввывод T: {T}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
