fee1c605f9b31895a2877e7c61ccae90e3d8cccd
Gera <Gera@511-05>
2026-09-19 13:23:53 +0200

    z bcghf

--- lab1_2/Program.cs
+++ lab1_2/Program.cs
@@ -6,9 +6,8 @@
     double L = double.Parse(Console.ReadLine());
     double T = 2 * Math.PI * Math.Sqrt(L * C);
     double v = 1 / T;
-
-
-    Console.WriteLine($"Ввывод T: {T}");
+    Console.WriteLine($"Вывод T: {T}");
+    Console.WriteLine($"Вывод v: {v}");
 }
 catch (Exception ex)
 {
---
+++ lad1-3/Program.cs
@@ -0,0 +1,48 @@
+﻿//Console.WriteLine($"{Math.PI:F2}");
+//Console.WriteLine($"{Math.E:F1}");
+//a = int.Parse( Console.ReadLine() );
+//Console.WriteLine($"Вы ввели число {a}");
+
+//    int a = int.Parse( Console.ReadLine() );
+//Console.WriteLine($"{a}-Вот такое число");
+//Console.WriteLine("1 13 49");
+//Console.WriteLine("7\t15\t100");
+//try
+//{
+//    Console.Write("Введите a:");
+//    double a = double.Parse(Console.ReadLine());
+//    Console.Write("Введите b");
+//    double b = double.Parse(Console.ReadLine());
+//    Console.Write("Введите c");
+//    double c = double.Parse(Console.ReadLine());
+//    double d = (-b + Math.Sqrt((b * b - 4 * a * c)) / (2 * a);
+//    Console.WriteLine($"{d:F2}");
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message );
+//}
+
+//try
+//{
+//    Console.Write("Введите количество сантиметров:");
+//    int sm = int.Parse(Console.ReadLine());
+//    int m = sm / 100;
+//    Console.WriteLine($"Полных метров{m}");
+//Console.Write("Введите количество секунд:");
+//int n = int.Parse(Console.ReadLine());
+//int hour = n / 3600;
+//int minute = n % 3600 / 60;
+//int second = n % 3600 % 60;
+//Console.WriteLine($"{ho}:{}:{}");
+//}
+//}
+//catch (Exception ex);
+//}
+//Console.WriteLine(ex.Message);
+//}
+//catch (Exception ex);
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
